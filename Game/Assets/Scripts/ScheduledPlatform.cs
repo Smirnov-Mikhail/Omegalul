f9085ea5aa85e922d0856dcc8a3b29dd50ce715a
Viuginov Nickolay <viuginov.nickolay@gmail.com>
2018-03-31 23:45:31 +0300

    rename

--- Game/Assets/Scripts/ScheduledPlatform.cs
+++ Game/Assets/Scripts/ScheduledPlatform.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class shaduledPlatform : MonoBehaviour {
+public class ScheduledPlatform : MonoBehaviour {
 
     private int MAX_STEP = 10;
 
