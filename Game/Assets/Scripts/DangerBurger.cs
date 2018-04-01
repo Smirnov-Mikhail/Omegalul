d2b76cd02819ec0993b976d5311c96cf69905cf7
Viuginov Nickolay <viuginov.nickolay@gmail.com>
2018-04-01 11:01:17 +0300

    mob flip reworked

--- Game/Assets/Scripts/DangerBurger.cs
+++ Game/Assets/Scripts/DangerBurger.cs
@@ -8,19 +8,38 @@ public class DangerBurger : MonoBehaviour
 
     private int count;
     private bool left;
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing
+        left = !left;
+
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+
+        var sprite = gameObject.GetComponentInChildren<SpriteRenderer>();
+        float width = sprite.bounds.max.x - sprite.bounds.min.x;
+        float delta = width;// / 2.0F;
+
+        Debug.Log(count);
+
+        if(left)
+        {
+            delta *= -1;
+        }
+
+        transform.localPosition = new Vector3(transform.localPosition.x + delta, transform.localPosition.y, transform.localPosition.z);
+    }
+
     void Update()
     {
         var curPos = transform.localPosition;
-        if (count == 0)
+        if (count == 0 || count == 100)
         {
-            left = true;
-            transform.localScale = new Vector3(-1.0F * transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        }
-
-        if (count == 100)
-        {
-            left = false;
-            transform.localScale = new Vector3(-1.0F * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            Flip();
         }
 
         if (left)
