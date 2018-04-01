75f6c1782fecac89c90d98922eef03ab0fea98c4
egor <egor.schavelev@gmail.com>
2018-04-01 13:37:00 +0300

    music)))

---
+++ Game/Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void Awake()
+    {
+        var comp = GetComponent<AudioSource>();
+        
+        comp.PlayDelayed(1);
+        Debug.Log("LOGGING!!");
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
